39b01e8316b81165affdd88d1e5de356c2194e67
Mc-Cheese-code <sidd.patty@gmail.com>
2025-03-10 23:14:49 +0530

    more tests and functions

--- poker/deck.py
+++ poker/deck.py
@@ -1,7 +1,12 @@
+import random
 class Deck():
     def __init__(self):
         self.cards = []
 
 
     def add_cards(self, card):
-        return self.cards.extend(card)     
+        return self.cards.extend(card) 
+
+
+    def shuffle(self):
+        random.shuffle(self.cards)    
